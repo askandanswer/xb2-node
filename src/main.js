26dd5e1750c0f076071ebfac8ec365919989235b
wujie <562104171@qq.com>
2020-05-05 21:48:18 +0800

    创建WEB服务

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-const greeting = 'hello ~';
-console.log(greeting);
+const http = require('http');
+const server = http.createServer((request, response) => {
+  console.log(request.headers["user-agent"])
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+  })
+  response.write(`<input />`);
+  response.end();
+});
+server.listen(3000, () => {
+  console.log('🚀 服务已启动');
+})
